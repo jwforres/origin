f5494e323de53bbb987481e3afc9819eeb3bbf37
Jessica Forrester <jforrest@redhat.com>
2016-01-27 14:36:21 -0500

    WIP - Example status extension

--- assets/extensions/extensions.js
+++ assets/extensions/extensions.js
@@ -9,3 +9,15 @@
  *
  * You can modify this file to test extensions in a development environment.
  */
+  $.getJSON("https://m0sg3q4t415n.statuspage.io/api/v2/summary.json",
+            function checkOpenIssues(data) {
+    var n = data.incidents.length;
+  
+    if (true /*n > 0*/) {
+      var outage = $('#outage');
+      $("<a href='http://status.openshift.com/' class='alert error' title='Track open issues on the OpenShift Online status page'>")
+        .text("" + n + " open issue" + ((n > 1) ? "s" : ""))
+        .appendTo(outage);
+      outage.show();
+    }
+  });
